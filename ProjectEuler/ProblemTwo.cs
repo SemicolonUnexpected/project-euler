aa5a92e15b2aafd8818a294321d4e84c1387484e
SemicolonUnexpected <mgracey1409@gmail.com>
2022-12-09 15:34:26 +0000

    Problem Two Completed
    Uses trial division to determine if each Fibonacci number is even and then adds it to the total if this is the case

---
+++ ProjectEuler/ProblemTwo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ProjectEuler {
+    class ProblemTwo : IExecutable {
+
+        public string Execute() {
+            return "The sum of all fibonacci numbers below 4,000,000 is " + SumOfFibonaccis(4000000);
+        }
+        
+        //Finds the sum of all even fibanacci numbers less than a number
+        long SumOfFibonaccis(long upperBound) {
+            var fibonacciNumbers = NumberGenerators.FibonacciNumbers();
+            long output = 0;
+
+            foreach (long fibonacciNumber in fibonacciNumbers) {
+                if (fibonacciNumber % 2 == 0) {
+                    output += fibonacciNumber;
+                }
+
+                if (fibonacciNumber > upperBound)
+                    break;
+            }
+
+            return output;
+        }
+        
+    }
+}
