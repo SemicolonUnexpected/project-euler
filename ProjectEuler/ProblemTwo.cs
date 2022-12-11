faecde74b31239c90b4b1208d45eefb71aa430eb
SemicolonUnexpected <mgracey1409@gmail.com>
2022-12-11 14:40:05 +0000

    Refactoring of Constants
    Constants removed where they were unnecessary and more concise formatting of the return in the Execute method

--- ProjectEuler/ProblemTwo.cs
+++ ProjectEuler/ProblemTwo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
 using System;
-using Number;
+using ProjectEuler.Number;
 
 namespace ProjectEuler {
     class ProblemTwo : IExecutable {
 
         public string Execute() {
-            return "The sum of all fibonacci numbers below 4,000,000 is " + SumOfFibonaccis(4000000);
+            return $"The sum of all fibonacci numbers below 4,000,000 is  + {SumOfFibonaccis(4000000)}";
         }
         
         //Finds the sum of all even fibanacci numbers less than a number
