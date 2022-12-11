8310852058ec59f13c68609ac1bc9541d9e99ee2
SemicolonUnexpected <mgracey1409@gmail.com>
2022-12-11 15:32:25 +0000

    Problem Four Completed
    Finds the largest palindrome that can be made from the product of two 3-digit numbers
    Name of method in palindrome class fixed

---
+++ ProjectEuler/ProblemFour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ProjectEuler.Number;
+
+namespace ProjectEuler {
+    class ProblemFour : IExecutable {
+
+        public string Execute() {
+            return $"The largest palindrome made from the product of two 3-digit numbers is {GetLargestThreeDigitDivisorPalindrome()}";
+        }
+
+        //Returns the largest palindrome with two three digit divisors
+        int GetLargestThreeDigitDivisorPalindrome() {
+            List<int> palindromes = new List<int>(Palindromes.GetSixDigitPalindromesDescending());
+            int output = 0;
+
+            foreach (int palindrome in palindromes) {
+                if (HasThreeDigitDivisors(palindrome)) {
+                    output = palindrome;
+                    break;
+                }
+            }
+
+            return output;
+        }
+
+        //Checks if a number has two three digit divisors
+        bool HasThreeDigitDivisors(int value) {
+
+            for (int i = 100; i * i < value; i++) {
+                if (value % i == 0 && value / i < 1000) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
