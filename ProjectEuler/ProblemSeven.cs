5eaaaa52aba34c6a6268b81e6423cfa7b44bad2a
SemicolonUnexpected <mgracey1409@gmail.com>
2022-12-11 15:56:57 +0000

    Problem Seven Completed
    Finds the 10001st prime by using a Sieve of Eratosthenes

---
+++ ProjectEuler/ProblemSeven.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ProjectEuler.Number;
+
+namespace ProjectEuler {
+    class ProblemSeven : IExecutable {
+
+		public string Execute() {
+			List<long> primes = Primes.GetSieveOfEratosthenes(10001);
+			return $"The 10,001st prime is " + primes[primes.Count() - 1];
+		}
+	}
+}
