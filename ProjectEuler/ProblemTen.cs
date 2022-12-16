739b90aad3e0df18cf2fbb7e9aca538914edd5b7
SemicolonUnexpected <mgracey1409@gmail.com>
2022-12-16 16:36:41 +0000

    Problem Ten Completed
    Finds the sum of all primes below 2000000 by using a Sieve of Eratosthenes

---
+++ ProjectEuler/ProblemTen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProjectEuler.Number;
+
+namespace ProjectEuler {
+	class ProblemTen : IExecutable {
+
+		public string Execute() {
+			List<long> primes = Primes.GetSieveOfEratosthenes(2000000);
+			return $"The sum of all the primes that are below 2000000 is {primes.Sum()}";
+		}
+	}
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 namespace ProjectEuler {
     class Program {
 
-        static readonly IExecutable problem = new ProblemNine();
+        static readonly IExecutable problem = new ProblemTen();
 
         static Stopwatch stopwatch = new Stopwatch();
 
