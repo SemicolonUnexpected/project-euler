59c2c7c6c8aad9045ae2cc94059473064b4a1f2b
SemicolonUnexpected <mgracey1409@gmail.com>
2022-12-23 16:52:06 +0000

    Problem Fourteen Completed
    Finds the longest chain produced by the Collatz sequence with a starting number under one million by brute force. Repeat division is prevented by the use of a dictionary and calculations begin at five hundred thousand

---
+++ ProjectEuler/ProblemFourteen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler {
+	class ProblemFourteen : IExecutable {
+
+		public string Execute() {
+			return $"The starting number, under one million, which produces the longest chain in the Collatz conjecture is {MaxCollatz(1000000)}";
+		}
+
+        //Finds the starting number under an upper bound that produces the longest chain before terminating
+        long MaxCollatz(long upperBound) {
+            Dictionary<long, long> collatzLengths = new Dictionary<long, long>();
+            collatzLengths.Add(1, 1);
+
+            long maxLength = 0;
+            long maxLengthBase = 0;
+
+            //If you can double the value and stay within the range, double the value will produce a longer chain
+            for (long i = upperBound / 2; i < upperBound + 1; i++) {
+                if (CollatzLength(i) > maxLength) {
+                    maxLength = CollatzLength(i);
+                    maxLengthBase = i;
+                }
+            }
+
+            return maxLengthBase;
+
+            //Finds the length of the chain recursively, avoiding recalculations with the help the collatzLengths dictionary
+            long CollatzLength(long number) {
+                if (collatzLengths.ContainsKey(number)) {
+                    return collatzLengths[number];
+                }
+                else if (number % 2 == 0) {
+                    number /= 2;
+                    return CollatzLength(number) + 1;
+                }
+                else {
+                    number *= 3;
+                    number++;
+                    number /= 2;
+                    return CollatzLength(number) + 2;
+                }
+            }
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 namespace ProjectEuler {
     class Program {
 
-        static readonly IExecutable problem = new ProblemThirteen();
+        static readonly IExecutable problem = new ProblemFourteen();
 
         static readonly Stopwatch stopwatch = new Stopwatch();
 
