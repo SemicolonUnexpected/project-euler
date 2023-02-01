35b4ac36f8da71492a95f1f2ec382df8dab32a38
SemicolonUnexpected <112770950+SemicolonUnexpected@users.noreply.github.com>
2023-02-01 20:32:16 +0000

    Problem Twenty One Completed
    Finds amicable numbers in time O(n * (ln(n) + 0.577) by an approach similar to the construction of a Sieve of Eratosthenes

---
+++ ProjectEuler/ProblemTwentyOne.cs
@@ -0,0 +1,30 @@
+﻿using ProjectEuler.Number;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler {
+    internal class ProblemTwentyOne : IExecutable {
+        public string Execute() {
+            string output = "";
+            List<long> amicableNumbers = GetAmicableNumbers();
+
+            foreach (var item in amicableNumbers) {
+                output += item + " ";
+            }
+            return $"The amicable numbers under 10,000 are... \n{output}\nTheir sum is {amicableNumbers.Sum()}";
+        }
+
+        List<long> GetAmicableNumbers() {
+            long[] sumOfProperDivisors = Primes.GetSumProperDivisors(10001);
+            List<long> output = new List<long>();
+
+            for (int i = 0; i < sumOfProperDivisors.Length; i++) {
+                if (sumOfProperDivisors[i] < sumOfProperDivisors.Length - 1 && i != sumOfProperDivisors[i] && i == sumOfProperDivisors[sumOfProperDivisors[i]]) {
+                    output.Add(i);
+                }
+            }
+
+            return output;
+        }
+    }
+}
