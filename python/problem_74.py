f16574b84b4a9a88552e513610c45cce159ccc00
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-02-01 21:05:45 +0000

    Added some stuff

--- python/problem_74.py
+++ python/problem_74.py
@@ -1,3 +1,6 @@
+# Possibly the slowest implementation ever but we balling
+# Could be improved by memoizing each cycle but idc
+# im so fed up with these first 100 problemss
 fact = {
         0: 1,
         1: 1,
@@ -14,7 +17,7 @@ fact = {
 
 def get_digits(n):
     digits = list()
-    while digits > 0:
+    while n > 0:
         digits.append(n % 10)
         n //= 10
     return digits
@@ -29,17 +32,22 @@ def digit_fact(n):
 
 
 def main():
-    cycles = dict()
+    count = 0
     for i in range(10**6):
-        path = dict()
+        path = set()
         length = 0
         n = i
         while True:
-            path[n] = length
+            path.add(n)
             length += 1
             n = digit_fact(n)
+            if n in path:
+                if length == 60:
+                    count += 1
+                break
             if length > 60:
                 break
+    print(count)
 
 
 if __name__ == '__main__':
