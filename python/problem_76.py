af7c91ede81d42a08c1c46531fa7353f494fef62
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-02-16 09:22:12 +0000

    Solved 76

--- python/problem_76.py
+++ python/problem_76.py
@@ -1,15 +1,21 @@
+from functools import cache
+
+
+@cache
 def count_partitions(num, upper):
-    if upper == 1 or num == 1:
+    if num < 0:
+        return 0
+    if num == 0:
         return 1
     else:
         sum = 0
-        for i in range(upper):
+        for i in range(1, upper + 1):
             sum += count_partitions(num - i, i)
         return sum
 
 
 def main():
-    print(count_partitions(5, 5))
+    print(count_partitions(100, 99))
 
 
 if __name__ == "__main__":
