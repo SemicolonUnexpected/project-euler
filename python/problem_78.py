844f323af955a7992ac393c055a1ad50d205439b
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-02-18 12:43:13 +0000

    78

---
+++ python/problem_78.py
@@ -0,0 +1,29 @@
+from functools import cache
+
+
+@cache
+def count_partitions(num, upper):
+    if num < 0:
+        return 0
+    if num == 0:
+        return 1
+    else:
+        sum = 0
+        for i in range(1, upper + 1):
+            sum += count_partitions(num - i, i)
+        return sum
+
+
+def main():
+    i = 0
+    while True:
+        i += 1
+        partitons = count_partitions(i, i)
+        print(i, partitons)
+        if partitons % 1_000_000 == 0:
+            print(i)
+            break
+
+
+if __name__ == "__main__":
+    main()
