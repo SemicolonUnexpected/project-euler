f0a21ba6f3c290173a430a02efc1fe7c2c7b7c59
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-06-20 07:06:55 +0100

    Started problem 80 and add rust

---
+++ python/problem_80.py
@@ -0,0 +1,26 @@
+# valid fractions are of the form n/d
+# => HCF(n/d) = 1,
+#    d <= 12000,
+#    1/3 < n/d < 1/2
+#
+# Fractions smaller than 1/6 will have
+# at least one non simplified member
+# in the range
+#
+# Fractions larger than 1/6 in the
+# range are... 2/5
+#
+# phi(n) = no. integers <= n that are
+# coprime with n
+#
+# d/3 < n < d/2
+#
+#
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
