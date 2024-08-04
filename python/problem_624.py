c304339d1943ea75088dd942e6a72022f01e2120
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-08-04 13:31:52 +0930

    Hopefully the beginnings of the solution implementation

--- python/problem_624.py
+++ python/problem_624.py
@@ -1,22 +1,50 @@
+from functools import cache
+
+
+@cache
 def mul_root(a, b, c, d):
     return ((a * c + 5 * b * d), (b * c + a * d))
 
 
-def bin_pow_root(a, b, n):
+@cache
+def bin_pow_root(a, b, power, modulus):
     res_a = 1
     res_b = 0
 
-    while n > 0:
-        if n & 1:
+    while power > 0:
+        if power & 1:
             res_a, res_b = mul_root(res_a, res_b,
                                     a, b)
+            res_a %= res_a
+            res_b %= res_b
         a, b = mul_root(a, b, a, b)
-        n >>= 1
+        a %= a
+        b %= b
+
+        power >>= 1
 
     return res_a, res_b
 
 
-#print(mul_root(1, 1, 1, 1))
-#print(mul_root(6, 2, 6, 2))
-#print(mul_root(56, 24, 56, 24))
-bin_pow_root(1, 1, 10**6)
+def main():
+    modulus = 1_000_000_009
+    power = 10**18
+
+    four_pow = pow(4, power, modulus)
+    four_double_pow = pow(four_pow, 2, modulus)
+
+    # (4√5 + 4)^x
+    a = bin_pow_root(4, 4, power, modulus)
+    # (-4√5 + 4)^x
+    b = bin_pow_root(-4, 4, power, modulus)
+
+    denominator = four_pow
+                  + four_double_pow
+                  - a
+                  - b
+
+    numerator = mul_root()
+
+
+if __name__ == "__main__":
+    main()
